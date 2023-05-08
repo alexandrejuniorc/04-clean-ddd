9926ab41870fa85b289a08609ab6e090df2e9157
Alexandre Junior <alexandrejunior408@gmail.com>
2023-05-08 15:18:55 -0300

    feat: mapeando relacionamentos

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,15 +1,19 @@
-import { Answer } from "../entities/answer"
+import { Answer } from "../entities/answer";
 
 interface AnswerQuestionUseCaseRequest {
-  instructorId: string
-  questionId: string
-  content: string
+  instructorId: string;
+  questionId: string;
+  content: string;
 }
 
 export class AnswerQuestionUseCase {
-  execute({instructorId, questionId, content}: AnswerQuestionUseCaseRequest) {
-    const answer = new Answer(content)
+  execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
+    const answer = new Answer({
+      content,
+      authorId: instructorId,
+      questionId,
+    });
 
-    return answer
+    return answer;
   }
 }
